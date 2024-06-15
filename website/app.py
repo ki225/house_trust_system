49189d8624d3a9a002b5150d3ebc4ac9902b2f95
yuan3x2 <132554952+yuan3x2@users.noreply.github.com>
2024-06-15 22:36:37 +0800

    Add files via upload

--- website/app.py
+++ website/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+FIXED_FILENAME = 'crack.png'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -14,5 +23,28 @@ def send_message():
     reply = f"I received your message: {user_message}"
     return jsonify({'reply': reply})
 
+@app.route('/submit', methods=['POST'])
+def upload_file():
+    city = request.form.get('City')
+    fault = request.form.get('Fault')
+    soil_liquefaction = request.form.get('Soil_Liquefaction')
+    land_subsidence = request.form.get('Land_Subsidence')
+    material = request.form.get('Material')
+    floor = request.form.get('Floor')
+
+    if 'image' not in request.files:
+        return 'No file part'
+    file = request.files['image']
+
+    if file.filename == '':
+        return 'No selected file'
+    if file and allowed_file(file.filename):
+        
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], FIXED_FILENAME)
+        file.save(file_path)
+        return app.send_static_file('index.html')
+    return 'File type not allowed'
+
 if __name__ == '__main__':
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     app.run(debug=True)
